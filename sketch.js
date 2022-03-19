ff5b384cb92d7b4eace782b57cdf16449f4a6e72
mayaBazz <87096736+mayaBazz@users.noreply.github.com>
2022-03-19 13:55:12 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var background;
+var cube;
+var orange;
+
+
+function preload(){
+  background = loadImage("background.png");
+  orange = loadImage("orange.png");
+}
+
+function setup() {
+  createCanvas(600,500);
+
+  
+  image(background, 0,0);
+
+  cube = createSprite(400, 200, 50, 50);
+  cube.addImage(orange);
+  cube.scale = 0.1
+  
+  
+  
+  
+}
+
+function draw() {
+  
+  
+  drawSprites();
+
+  if (keyDown(UP_ARROW)){
+    cube.y -=5;
+  }
+  if (keyDown(DOWN_ARROW)){
+    cube.y +=5;
+  }
+  if (keyDown(RIGHT_ARROW)){
+    cube.x +=5;
+  }
+  if (keyDown(LEFT_ARROW)){
+    cube.x -=5;
+  }
+}
